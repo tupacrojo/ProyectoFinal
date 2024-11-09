793f6cfec65030f3c789adc9ae2ec1ba3a505800
juanCruz <juanx.martel@gmail.com>
2024-11-09 17:03:27 -0300

    sigo sumando comiits

--- src/componentes/menus/supervisor/supervisor.component.ts
+++ src/componentes/menus/supervisor/supervisor.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ListaPedidosComponent } from "../../pedidos/lista-pedidos/lista-pedidos.component";
 
 @Component({
   selector: 'app-supervisor',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, ListaPedidosComponent],
   templateUrl: './supervisor.component.html',
   styleUrl: './supervisor.component.css'
 })
