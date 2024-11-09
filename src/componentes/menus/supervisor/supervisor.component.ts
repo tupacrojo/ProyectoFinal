9810236de5f9850ecd3a93e2d0cff3d6d42d2a1a
Lopee12 <nlopezosornio@gmail.com>
2024-11-09 19:35:10 -0300

    Juan, acomodame la tabla

--- src/componentes/menus/supervisor/supervisor.component.ts
+++ src/componentes/menus/supervisor/supervisor.component.ts
@@ -1,14 +1,37 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ListaPedidosComponent } from "../../pedidos/lista-pedidos/lista-pedidos.component";
+import { Pedido } from '../../../interfaces/Pedido.interface';
+import { PedidoService } from '../../../services/pedido.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-supervisor',
   standalone: true,
-  imports: [RouterModule, ListaPedidosComponent],
+  imports: [RouterModule,CommonModule],
   templateUrl: './supervisor.component.html',
   styleUrl: './supervisor.component.css'
 })
 export class SupervisorComponent {
+  
+  constructor(private listaPedidosService: PedidoService) {}
+  listaPedidos: Pedido[] = [];
+
+
+  ngOnInit(): void{ 
+      this.mostrarLista();
+  }
+
+  mostrarLista() {
+    this.listaPedidosService.getListaPedidos().subscribe({
+      next: (pedido) => {
+        this.listaPedidos = pedido;
+      },
+
+      error: (err) => {
+        console.log('Error', err);
+      },
+    });
+  }
 
 }
