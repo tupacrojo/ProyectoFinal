f1ed3d4ad95006e4fbec706e4860467c489f1c62
tupacrojo <72628251+tupacrojo@users.noreply.github.com>
2024-11-08 23:45:10 -0300

    molesto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ListaProductosComponent } from '../componentes/productos/lista-productos/lista-productos.component';
-import { NuevoProductoComponent } from '../componentes/productos/nuevo-producto/nuevo-producto.component';
-import { AdministradorComponent } from '../componentes/menus/administrador/administrador.component';
-import { ListaVentasComponent } from '../componentes/ventas/lista-ventas/lista-ventas.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    CommonModule,
-    ListaProductosComponent,
-    NuevoProductoComponent,
-    AdministradorComponent,
-    ListaVentasComponent,
-  ],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
