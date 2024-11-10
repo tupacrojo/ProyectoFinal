64042c863e73abef18079161f4a567807dc31571
tupacrojo <72628251+tupacrojo@users.noreply.github.com>
2024-11-10 02:10:21 -0300

    pepe

--- src/componentes/menus/supervisor/supervisor.component.ts
+++ src/componentes/menus/supervisor/supervisor.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { ListaPedidosComponent } from "../../pedidos/lista-pedidos/lista-pedidos.component";
 import { Pedido } from '../../../interfaces/Pedido.interface';
 import { PedidoService } from '../../../services/pedido.service';
 import { CommonModule } from '@angular/common';
@@ -8,18 +7,16 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-supervisor',
   standalone: true,
-  imports: [RouterModule,CommonModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './supervisor.component.html',
-  styleUrl: './supervisor.component.css'
+  styleUrl: './supervisor.component.css',
 })
 export class SupervisorComponent {
-  
   constructor(private listaPedidosService: PedidoService) {}
   listaPedidos: Pedido[] = [];
 
-
-  ngOnInit(): void{ 
-      this.mostrarLista();
+  ngOnInit(): void {
+    this.mostrarLista();
   }
 
   mostrarLista() {
@@ -34,34 +31,32 @@ export class SupervisorComponent {
     });
   }
 
-  modficarPedido(ped: Pedido){
+  modficarPedido(ped: Pedido) {
     this.listaPedidosService.putPedido(ped).subscribe({
-      next:() => {
-        console.log("Actualizado el estado correctamente");
-      }, 
+      next: () => {
+        console.log('Actualizado el estado correctamente');
+      },
       error: (err) => {
         console.log(err.message);
-      }
-    })
+      },
+    });
   }
 
-
-  valorEstado(valorEstado: string):boolean{
-    if(valorEstado === "En espera de confirmacion") return true;
+  valorEstado(valorEstado: string): boolean {
+    if (valorEstado === 'En espera de confirmacion') return true;
 
     return false;
   }
 
-  cambiarEstadoAceptacion(index: number){
-    this.listaPedidos[index].estado = "En preparacion";
+  cambiarEstadoAceptacion(index: number) {
+    this.listaPedidos[index].estado = 'En preparacion';
 
     this.modficarPedido(this.listaPedidos[index]);
   }
 
-  cambiarEstadoRechazo(index: number){
-    this.listaPedidos[index].estado = "Rechazado";
+  cambiarEstadoRechazo(index: number) {
+    this.listaPedidos[index].estado = 'Rechazado';
 
     this.modficarPedido(this.listaPedidos[index]);
   }
-
 }
