b8067048fb0bcb7bc8dc131ea38a4d498df6c781
tupacrojo <72628251+tupacrojo@users.noreply.github.com>
2024-11-10 21:48:25 -0300

    estilos

--- src/componentes/menus/encargado/encargado.component.ts
+++ src/componentes/menus/encargado/encargado.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-encargado',
   standalone: true,
   imports: [RouterModule],
   templateUrl: './encargado.component.html',
-  styleUrl: './encargado.component.css'
+  styleUrl: './encargado.component.css',
 })
 export class EncargadoComponent {
-
+  authService = inject(AuthService);
+  logout() {
+    this.authService.logout();
+  }
 }
