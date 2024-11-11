b679c88bc0f4fd01132eca4819399f722425845f
tupacrojo <72628251+tupacrojo@users.noreply.github.com>
2024-11-10 22:04:52 -0300

    cerrar sesion

--- src/componentes/menus/administrador/administrador.component.ts
+++ src/componentes/menus/administrador/administrador.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-administrador',
   standalone: true,
   imports: [RouterModule],
   templateUrl: './administrador.component.html',
-  styleUrl: './administrador.component.css'
+  styleUrl: './administrador.component.css',
 })
 export class AdministradorComponent {
-
+  authService = inject(AuthService);
+  logout() {
+    this.authService.logout();
+  }
 }
