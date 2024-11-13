d70248872576960db63f6abe2507e9cf456515db
tupacrojo <72628251+tupacrojo@users.noreply.github.com>
2024-11-13 00:28:37 -0300

    navbar condicional

--- src/componentes/ui/side-navbar/side-navbar.component.ts
+++ src/componentes/ui/side-navbar/side-navbar.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
-import { ThemeSwitcherComponent } from "../theme-switcher/theme-switcher.component";
-import { RoleViewerComponent } from "../role-viewer/role-viewer.component";
+import { Component, OnInit } from '@angular/core';
+import { ThemeSwitcherComponent } from '../theme-switcher/theme-switcher.component';
+import { RoleViewerComponent } from '../role-viewer/role-viewer.component';
+import { ROLES } from '../../../enum/roles';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-side-navbar',
   standalone: true,
   imports: [ThemeSwitcherComponent, RoleViewerComponent],
   templateUrl: './side-navbar.component.html',
-  styleUrl: './side-navbar.component.css'
+  styleUrl: './side-navbar.component.css',
 })
-export class SideNavbarComponent {
+export class SideNavbarComponent implements OnInit {
+  ROLES = ROLES;
+  actualRole: string = '';
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.actualRole = this.authService.getActiveRole() || '';
+  }
 }
